d722a3752e12015f06f3e1753351adb8b3d212fe
Huy Nguyen <huynguyen260398@gmail.com>
2023-04-22 10:30:00 +0700

    Added dbcontext and auth services

---
+++ DotNet7_WebAPI_JWT/Core/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet7_WebAPI_JWT.Core.DbContext;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
